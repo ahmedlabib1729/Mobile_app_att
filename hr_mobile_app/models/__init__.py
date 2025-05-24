c36a2cbe3f44b9806707e4905e44d08aa6350051
ahmedlabib1729 <150477629+ahmedlabib1729@users.noreply.github.com>
2025-05-25 00:03:49 +0400

    Add files via upload

---
+++ hr_mobile_app/models/__init__.py
@@ -0,0 +1,6 @@
+from . import hr_employee
+from . import res_config_settings
+from . import hr_mobile_app_settings
+from . import hr_attendance
+from . import hr_leave_mobile
+from . import hr_announcement
