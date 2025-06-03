7bf0e916553ebc96f2453efa80a971a14373a12c
ahmedlabib1729 <150477629+ahmedlabib1729@users.noreply.github.com>
2025-06-03 21:34:18 +0400

    Delete club directory

--- club/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import player
-from . import parent
-from . import sport
-from . import hr_employee
-from . import subscription
-from . import payment_receipt
-from . import subscription_request
--- club/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import payment_wizard
-from . import subscription_request_reject_wizard
