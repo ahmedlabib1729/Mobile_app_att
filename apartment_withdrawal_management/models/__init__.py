9bd2225fd2953ea79e13df4a7b0d4ead5260b8ec
ahmedlabib1729 <150477629+ahmedlabib1729@users.noreply.github.com>
2025-06-10 20:29:14 +0400

    Delete apartment_withdrawal_management directory

--- apartment_withdrawal_management/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_config_settings
-from . import withdrawal_monitor
-from . import ownership_contract
-from . import building_unit
-from . import withdrawal_report
-from . import installment_payment_fix
