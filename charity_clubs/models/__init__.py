6441ce06a5c8911efca68a43c05d04f63cae5e5c
ahmedlabib1729 <150477629+ahmedlabib1729@users.noreply.github.com>
2025-06-15 03:02:42 +0400

    Add files via upload

---
+++ charity_clubs/models/__init__.py
@@ -0,0 +1,8 @@
+from . import headquarters
+from . import departments
+from . import clubs
+from . import student_profile
+from . import member_profile  # نموذج جديد للعضوات
+from . import club_registrations
+from . import booking_registrations
+from . import ladies_programs
