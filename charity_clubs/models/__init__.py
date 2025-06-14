e3381d3ea99724e8ccce9edd899f342594af3a23
ahmedlabib1729 <150477629+ahmedlabib1729@users.noreply.github.com>
2025-06-14 11:07:03 +0400

    Delete charity_clubs directory

--- charity_clubs/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import headquarters
-from . import departments
-from . import clubs
-from . import student_profile
-from . import member_profile  # نموذج جديد للعضوات
-from . import club_registrations
-from . import booking_registrations
