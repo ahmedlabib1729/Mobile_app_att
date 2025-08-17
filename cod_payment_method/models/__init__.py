65dfbceed6dfd06f5255e3083ccd4406a642256a
ahmedlabib1729 <150477629+ahmedlabib1729@users.noreply.github.com>
2025-08-17 16:05:21 +0400

    Delete cod_payment_method directory

--- cod_payment_method/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import const
-from . import payment_cod
-from . import payment_provider
-from . import payment_method
-from . import sale_order
-from . import res_country
