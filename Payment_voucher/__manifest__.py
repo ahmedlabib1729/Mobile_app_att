f0b94ef92a0bd514ae64e602a6838bc28af7aa53
ahmedlabib1729 <150477629+ahmedlabib1729@users.noreply.github.com>
2025-09-09 01:06:46 +0400

    Add files via upload

---
+++ Payment_voucher/__manifest__.py
@@ -0,0 +1,18 @@
+
+{
+'name' : "Payment Receipt Form" ,
+'author' : "Ahmed Labib",
+
+'depends' : ['base', 'account' ],
+'data' : [
+
+        'report/ir_actions_report.xml',
+        'report/payment_receipt_form.xml',
+        'report/layout.xml',
+        'report/layout_cash.xml',
+        'report/Cash_receipt_form.xml',
+        'report/Cash_receipt_form2.xml',
+
+        ],
+
+}
