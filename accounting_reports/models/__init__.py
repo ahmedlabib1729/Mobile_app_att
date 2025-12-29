be5f3c6e766f68fa7ee975fc2bc2cc136fc8e6a3
ahmedlabib1729 <150477629+ahmedlabib1729@users.noreply.github.com>
2025-12-29 13:31:26 +0400

    Delete accounting_reports directory

--- accounting_reports/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import general_ledger
-from . import partner_ledger
-from . import trial_balance
-from . import aged_partner
-from . import tax_report
-from . import profit_loss
-from . import balance_sheet
-from . import aged_partner
